bfbb4963eadb1044badbfc129b5a9391661d41f3
iepbunleng016bossiercity@gmail.com <iepbunleng016bossiercity@gmail.com>
2020-11-22 22:57:09 -0600

    add all file to git

---
+++ HelloWorld1/GreetPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HelloWorld1
+{
+    public partial class GreetPage : ContentPage
+    {
+        public GreetPage()
+        {
+            InitializeComponent();
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            DisplayAlert("Title","Alert me","OK");
+        }
+    }
+}
